fe40a6be44a3a104df174c47266d345b1140819f
joyaljoseph23 <joyaljjosephh@gmail.com>
2025-01-28 05:08:11 +0530

    application db context added

---
+++ GreenChillyShop.DataAccess/ApplicationDbContext.cs
@@ -0,0 +1,30 @@
+﻿using GreenChillyShop.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreenChillyShop.DataAccess
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options):base(options)
+        {
+            
+        }
+        public DbSet<Product>Products { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().HasData(
+                new Product { Id=1,Category="Vegetables",Description="This sections contains vegetables",Name="Carrot",Price=2,StockQuantity=50},
+                new Product { Id = 2, Category = "Fruits", Description = "This sections contains Fruits", Name = "Banana", Price = 50, StockQuantity = 150 },
+                new Product { Id = 1, Category = "Sweets", Description = "This sections contains sweets", Name = "Sona papdi", Price = 112, StockQuantity = 50 }
+
+
+                );
+        }
+    }
+}
---
+++ GreenChillyShop.Web/Controllers/ProductController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GreenChillyShop.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            //var products=await 
+            return View();
+        }
+    }
+}
