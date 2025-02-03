ffe4b50926e8d114d7802e6e73e0619864405b2b
joyaljoseph23 <joyaljjosephh@gmail.com>
2025-02-03 13:27:42 +0530

    UpdatedOne with db connection

--- GreenChillyShop.DataAccess/ApplicationDbContext.cs
+++ GreenChillyShop.DataAccess/ApplicationDbContext.cs
@@ -20,9 +20,9 @@ namespace GreenChillyShop.DataAccess
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Product>().HasData(
-                new Product { Id=1,Category="Vegetables",Description="This sections contains vegetables",Name="Carrot",Price=2,StockQuantity=50},
+                new Product { Id = 1, Category = "Vegetables", Description = "This sections contains vegetables", Name = "Carrot", Price = 2, StockQuantity = 50 },
                 new Product { Id = 2, Category = "Fruits", Description = "This sections contains Fruits", Name = "Banana", Price = 50, StockQuantity = 150 },
-                new Product { Id = 1, Category = "Sweets", Description = "This sections contains sweets", Name = "Sona papdi", Price = 112, StockQuantity = 50 }
+                new Product { Id = 3, Category = "Sweets", Description = "This sections contains sweets", Name = "Sona papdi", Price = 112, StockQuantity = 50 }
 
 
                 );
--- GreenChillyShop.Web/Controllers/ProductController.cs
+++ GreenChillyShop.Web/Controllers/ProductController.cs
@@ -1,13 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using GreenChillyShop.DataAccess;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace GreenChillyShop.Web.Controllers
 {
     public class ProductController : Controller
     {
+        private readonly ApplicationDbContext _context;
+        public ProductController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
         public async Task<IActionResult> Index()
         {
-            //var products=await 
-            return View();
+            var products=await _context.Products.ToListAsync();
+            return View(products);
         }
     }
 }
